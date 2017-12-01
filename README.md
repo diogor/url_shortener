a7925e601aa43b8e7a9998fb86fe59f10f1f6f3f
Diogo Lima <diogooo@gmail.com>
2017-12-01 10:45:17 -0200

    inicial

---
+++ README.md
@@ -0,0 +1,7 @@
+# API Encurtador de URL
+
+## Instalação
+> É recomendado o uso de Python3
+``` bash
+pip install -r requirements.txt
+```
---
+++ run.py
@@ -0,0 +1,57 @@
+import random, string
+import datetime
+from bottle import template, get, post, run, response, request, redirect, default_app
+from tinydb import TinyDB, Query
+
+DATABASE = 'database.json'
+
+app = application = default_app()
+
+def clean_db():
+    db = TinyDB(DATABASE)
+    query = Query()
+    d = datetime.timedelta(days=2)
+    r = datetime.datetime.utcnow() - d
+    res = db.search(query.date < r.timestamp())
+    db.remove(doc_ids=[x.doc_id for x in res])
+
+@app.get('/')
+@app.get('/<token>')
+def index(token=None):
+    if not token:
+        response.status = 400
+        return {"message": "What are you doing, son?"}
+    db = TinyDB(DATABASE)
+    query = Query()
+    res = db.search(query.token == token.upper())
+    
+    if not res:
+        response.status = 404
+        return {"message": "Link not found!"}
+    
+    if len(res) > 1:
+        return {
+        "message": "You are lucky! A colision occurred. There is more than one URL with {} as id."
+        .format(token),
+        "uris": ", ".join([x['uri'] for x in res])
+        }
+    
+    return redirect(res[0]['uri'])
+
+
+@app.post('/short/')
+def short():
+    uri = request.params.get('uri') or request.json.get('uri')
+    if not uri:
+        response.status = 400
+        return {"message": "'uri' parameter is required you dumb!"}
+    token = "".join([random.choice(string.hexdigits) for n in range(4)]).upper()
+    db = TinyDB(DATABASE)
+    link = "https://{}/{}".format(request.get_header('host'), token)
+    db.insert({'token': token, 'uri': uri, 'date': datetime.datetime.utcnow().timestamp()})
+    
+    clean_db()
+    return {"link": link}
+
+if __name__ == "__main__":
+    run(host='localhost', port=8080)
